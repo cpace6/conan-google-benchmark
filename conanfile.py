c935c028aa6940e8d99f7656c66314039a1c055a
KEYSIGHT\chripace <christopher.pace@keysight.com>
2016-12-02 14:11:24 -0500

    removed duplicate build function

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,6 @@ class GoogleBenchmarkConan(ConanFile):
     name = "google-benchmark"
     version = "1.1.0"
     settings = "arch", "build_type", "compiler", "os"
-    # options = {"option1": [,], } <-- any options needed?
     generators = "cmake"
     url = "http://github.com/cpace6/conan-google-benchmark"
     source_root = "benchmark-1.1.0"
@@ -25,15 +24,6 @@ class GoogleBenchmarkConan(ConanFile):
         self.run("mkdir _build")
         configure_command = 'cd _build && cmake ../%s %s' % (self.source_root, cmake.command_line)
         self.run(configure_command)
-        self.run("cd _build && cmake --build . %s" % cmake.build_config)
-
-    def build(self):
-        cmake = CMake(self.settings)
-        if self.settings.os == "Windows":
-            self.run("IF not exist _build mkdir _build")
-        else:
-            self.run("mkdir _build")
-        self.run('cd _build && cmake ../%s %s' % (self.source_root, cmake.command_line))
         self.run("cd _build && cmake --build . %s" % cmake.build_config)
 
     def package(self):
