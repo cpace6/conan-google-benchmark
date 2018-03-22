69f68a765e9330edc1a26ae1f894723958391c1f
Raúl Bocanegra Algarra <raul.bocanegra.algarra@gmail.com>
2018-03-23 00:36:44 +0100

    Update for conan 1.0.0 and google-benchmark 1.3.0

--- conanfile.py
+++ conanfile.py
@@ -1,36 +1,41 @@
-from conans import ConanFile, CMake
-from conans.tools import download, unzip, replace_in_file
+from conans import ConanFile, CMake, tools
+import os, shutil
 
-
-class GoogleBenchmarkConan(ConanFile):
+class GooglebenchmarkConan(ConanFile):
     name = "google-benchmark"
-    version = "1.1.0"
-    settings = "arch", "build_type", "compiler", "os"
+    version = "1.3.0"
+    license = "https://github.com/raulbocanegra/conan-google-benchmark"
+    url = "https://github.com/raulbocanegra/conan-google-benchmark"
+    description = "A library to support the benchmarking of functions, similar to unit-tests."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     generators = "cmake"
-    url = "http://github.com/cpace6/conan-google-benchmark"
-    source_root = "benchmark-1.1.0"
-    license = "https://github.com/google/benchmark/blob/master/LICENSE"
 
     def source(self):
-        zip_name = "v%s.zip" % self.version
+        zip_name = "v%s.zip" % self.version        
         url = "https://github.com/google/benchmark/archive/%s" % zip_name
-        download(url, zip_name)
-        unzip(zip_name)
-        replace_in_file('{:s}/src/CMakeLists.txt'.format(self.source_root),
-                              'if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")\n  target_link_libraries(benchmark Shlwapi)\nendif()',
-                              'if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")\n  target_link_libraries(benchmark Shlwapi)\nelse()\n if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")\n  find_library(LIB_RT rt)\n  target_link_libraries(benchmark ${LIB_RT})\n endif()\nendif()')
-
+        tools.download(url, zip_name)
+        tools.unzip(zip_name)
+        shutil.move("benchmark-%s" % self.version, "benchmark")
+        tools.replace_in_file("benchmark/CMakeLists.txt", "project (benchmark)", '''project (benchmark)\ninclude(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\nconan_basic_setup()''')        
+        os.unlink(zip_name)        
+        
+    def build_requirements(self):
+        self.build_requires("gtest/1.8.0@bincrafters/stable")    
+    
     def build(self):
-        cmake = CMake(self.settings)
-        self.run("mkdir _build")
-        configure_command = 'cd _build && cmake ../%s %s' % (self.source_root, cmake.command_line)
-        self.run(configure_command)
-        self.run("cd _build && cmake --build . %s" % cmake.build_config)
-
+        cmake = CMake(self)
+        cmake.configure(source_folder="benchmark")
+        cmake.build()
+        print("build=%s"%self.build_folder)
     def package(self):
-        self.copy(pattern="*.h", dst="include", src="%s/include" % self.source_root, keep_path=True)
-        self.copy(pattern="*.a", dst="lib", src=".", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src=".", keep_path=False)
+        self.copy("*.h", dst="include", src="benchmark/include", keep_path=True)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["benchmark"]
