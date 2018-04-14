1f244ff2b40f64e071fa1d0baba520a0090bd746
Raúl Bocanegra Algarra <raul.bocanegra.algarra@gmail.com>
2018-04-14 12:39:42 +0200

    Fix compilation on AppVeyor

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, tools
-import os, shutil
+import os
 
 class GooglebenchmarkConan(ConanFile):
     name = "google-benchmark"
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,20 +9,11 @@ import os
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    default_options = "google-benchmark:shared=False"
-    def requirements(self):
-        self.requires("google-benchmark/1.3.0@rboc/testing")
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
         cmake.configure()
         cmake.build()
-
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
